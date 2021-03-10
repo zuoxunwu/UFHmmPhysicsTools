200d3d15e92ff0dcd35c22abb112530dce0b26c6
jrotter2 <john.rotter@ufl.edu>
2021-03-10 20:05:34 +0100

    Changed hmmAnalyzer.py to be a module instead of standalone postprocessor

--- python/analyzers/hmmAnalyzer.py
+++ python/analyzers/hmmAnalyzer.py
@@ -1,9 +1,6 @@
+
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
-from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
-from importlib import import_module
-import os
-import sys
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
@@ -22,14 +19,10 @@ class hmmAnalyzer(Module):
         self.addObject(self.h_dimu_mass)
 
         pass
-        
+
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
-        # electrons = Collection(event, "Electron")
         muons = Collection(event, "Muon")
-        # jets = Collection(event, "Jet")
-        # eventSum = ROOT.TLorentzVector()
-        # print "n muons: %d" % len(muons)
         self.h_nMu.Fill(len(muons))
         nMu = 0
         for mu in muons:
@@ -43,18 +36,9 @@ class hmmAnalyzer(Module):
 
 
 
-        # self.out.fillBranch("EventMass", eventSum.M())
         return True
 
 
 # define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
-
-# hmmAnalyzerConstr = lambda: hmmAnalyzer()
-
-# preselection = "Jet_pt[0] > 250"
-files = ["root://cms-xrd-global.cern.ch//store/mc/RunIIAutumn18NanoAODv7/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/NANOAODSIM/Nano02Apr2020_102X_upgrade2018_realistic_v21-v1/100000/4FA4D948-38F7-6A4C-BBD6-33FA5A37EEA9.root"]
-p = PostProcessor(".", files, cut="0==0", branchsel="../../UFHmmPhysicsTools/analyzers/keep_and_drop_input.txt", modules=[
-                  hmmAnalyzer()], noOut=True, histFileName="histOut.root", histDirName="plots", maxEntries=1000)
-p.run()
-
+#hmmAnalyzer = lambda: hmmAnalyzer()
 
